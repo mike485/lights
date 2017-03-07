de4d0fbb8a87aad9fd0c1acd7c3bcbeda474ccfd
mike485 <michaelgknox@gmail.com>
2017-03-07 15:20:08 -0500

    Added redPin and greenPin checkBox with start and stop

--- GUILedPro.py
+++ GUILedPro.py
@@ -26,49 +26,59 @@ redPin = board.get_pin('d:11:p')
 top = Tkinter.Tk()
 top.title("Button for Pin Action")
 top.minsize(300, 30)
+redVar = Tkinter.IntVar()
+greenVar = Tkinter.IntVar()
+
 
 
 # create button
 
 def onStartButtonPress():
-    timePeriod = timePeriodEntry.get()
-    timePeriod = float(timePeriod)
-    ledBrigtness = brightnessScale.get()
-    ledBrightness = float()
+    #timePeriod = timePeriodEntry.get()
+    #timePeriod = float(timePeriod)
+    #ledBrigtness = brightnessScale.get()
+    #ledBrightness = float()
     startButton.config(state=Tkinter.DISABLED)
     #pin.write(ledBrightness/100.0)
-    pin.write(1)
+    #pin.write(1)
     #Led will be on for a specific amount of time listed below
-    sleep(timePeriod)
-    pin.write(0)
+    #sleep(timePeriod)
+    #pin.write(0)
     startButton.config(state=Tkinter.ACTIVE)
+    redPin.write(redVar.get())
+    greenPin.write(greenVar.get())
 
-
+def onStopButtonPress():
+    redPin.write(0)
+    greenPin.write(0)
                                 
-timePeriodEntry = Tkinter.Entry(top, bd=5, width=25)
-brightnessScale = Tkinter.Scale(top, from_=0, to=100, orient=Tkinter.HORIZONTAL)                                
+#timePeriodEntry = Tkinter.Entry(top, bd=5, width=25)
+#brightnessScale = Tkinter.Scale(top, from_=0, to=100, orient=Tkinter.HORIZONTAL)                                
 startButton = Tkinter.Button(top, text = "Start", command=onStartButtonPress)
+stopButton = Tkinter.Button(top, text = "Stop", command=onStopButtonPress)
 exitButton = Tkinter.Button(top, text = "Exit", command=quit)
-
+redCheckBox = Tkinter.Checkbutton(top, text = "Red LED", variable=redVar)
 greenCheckBox = Tkinter.Checkbutton(top, text = "Green light", variable=greenVar)
                         
                                 
 # .pack() makes it available in the main window
 
-timePeriodEntry.grid(column=1,row=1)                                
+#timePeriodEntry.grid(column=1,row=1)                                
 #timePeriodEntry.pack()
-brightnessScale.grid(column=1, row=2)
+#brightnessScale.grid(column=1, row=2)
 #brightnessScale.pack() 
 
 #can manually shift the focus of the graphical pointer to the timePeriodEntry widget
 #using the focus_set method, it obtains the value from the timePeriodEntry object and convert
 # it into a float value using using the float fuction()
 
-timePeriodEntry.focus_set()
-Tkinter.Label(top, text="Time (seconds)").grid(column=2, row=1)
-Tkinter.Label(top, text="Brightness (%)").grid(column=2, row=2)
-
+#timePeriodEntry.focus_set()
+#Tkinter.Label(top, text="Time (seconds)").grid(column=2, row=1)
+#Tkinter.Label(top, text="Brightness (%)").grid(column=2, row=2)
 startButton.grid(column=1, row=3)
+stopButton.grid(column=3, row=3)
+redCheckBox.grid(column=1, row=1)
+greenCheckBox.grid(column=2, row=1)
 exitButton.grid(column=2, row=3)
 
 #startButton.pack()
@@ -78,7 +88,7 @@ exitButton.grid(column=2, row=3)
 # the pack geometry manager organizes the widgets into rows and columns
 #hellolabel.pack()
 # Start and open the Window
-# Test update 2
+
 
 top.mainloop()
 
